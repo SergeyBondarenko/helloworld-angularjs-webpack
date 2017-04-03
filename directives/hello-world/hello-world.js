0de2b02a1eccb063311cd008629b2f3a9c0f25d2
Sergey Bondarenko <sergibondarenko@gmail.com>
2017-04-03 07:59:05 +0200

    Create directive.

---
+++ directives/hello-world/hello-world.js
@@ -0,0 +1,16 @@
+module.exports = function (ngModule) {
+  ngModule.directive('helloWorld', helloWorldFn);
+
+  function helloWorldFn () {
+    return {
+      restrict: 'E',
+      scope: {},
+      templateUrl: 'directives/hello-world/hello-world.html',
+      controllerAs: 'vm',
+      controller: function () {
+        var vm = this;
+        vm.greeting = 'Hello, World!';
+      }
+    }
+  }
+};
