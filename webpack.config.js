bada17d1f5fe26907941dbb973f2a636ded590df
Sergey Bondarenko <sergibondarenko@gmail.com>
2017-04-04 08:01:59 +0200

    Add less loader.

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,8 @@ module.exports = {
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.html$/, loader: 'raw-loader', exclude: /node_modules/ },
       { test: /\.css$/, loader: 'style-loader!css-loader', exclude: /node_modules/ },
-      { test: /\.styl$/, loader: 'style-loader!css-loader!stylus-loader', exclude: /node_modules/ }
+      { test: /\.styl$/, loader: 'style-loader!css-loader!stylus-loader', exclude: /node_modules/ },
+      { test: /\.less$/, loader: 'style-loader!css-loader!less-loader', exclude: /node_modules/ }
     ]
   }
 };
