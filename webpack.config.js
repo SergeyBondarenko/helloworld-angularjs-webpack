e5c001db56abaf3cca9c324b787bfb5436781f06
Sergey Bondarenko <sergibondarenko@gmail.com>
2017-04-05 07:14:22 +0200

    Move bundle to app/bin

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ module.exports = {
   entry: './index.js',
   output: {
     path: __dirname + '/app',
-    filename: './bundle.js'
+    filename: './bin/bundle.js'
   },
   module: {
     loaders: [
